cc1804b32643cd2a00d82a65ae9a1a45d44e3d0f
Nitin Mali <nitin25mali93@gmail.com>
2021-07-15 14:11:48 +0530

    future continuations

--- futures/futurescontinuations.py
+++ futures/futurescontinuations.py
@@ -4,13 +4,13 @@ plt.style.use('seaborn-darkgrid')
 #data manipulation lib
 import pandas as pd
 #path to read data files
-path="../data_modules/"
+path="./data_modules/"
 
 #read the data
 #First Contract
-first_contract_data=pd.read.csv( path+"HEV20.csv", index_column=0,parse_dates=True)
+first_contract_data=pd.read_csv( path+"HEV20.csv", index_col=0, parse_dates=True)
 #Second Contract
-second_contract_data=pd.read.csv(path+"HEZ20.csv", index_column=0,parse_dates=True)
+second_contract_data=pd.read_csv(path+"HEZ20.csv", index_col=0, parse_dates=True)
 #define the plot size
 plt.figure(figsize=(8,6))
 plt.title('Unadjusted Futures Price',fontsize=12)
@@ -51,4 +51,24 @@ continuous_futures_proportional=\
 #Multiply the adjustment factor to the continuous series till the expiry of the first contract
 continuous_futures_proportional.loc[:first_contract_expiry] *=adjustment_factor
 
+continuous_futures_proportional=\
+    continuous_futures_proportional.append(second_contract_data.loc[first_contract_expiry:].Settle)
 
+#Print the prices
+print("The price of first contract on{} is ${}.".format(first_contract_expiry,first_contract_on_expiry))
+
+print("The price of the second contract on {} is ${}".format(first_contract_expiry,second_contract_on_rollover))
+
+print("The adjustment factor to multiply to the first contract is{}." .format(round(adjustment_factor,3)))
+
+#Define the plot size
+plt.figure(figsize=(8,7))
+plt.title('Adjusted Futures Contracts', fontsize=14)
+plt.xlabel('Year-Month',fontsize=12)
+plt.ylabel('Price',fontsize=12)
+
+#Plotting the continous contract
+plt.plot(continuous_futures_proportional)
+
+#show the plot
+plt.show()
